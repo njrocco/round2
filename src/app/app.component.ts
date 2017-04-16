4fa9dbc43189934b06823da0cf3d8996097b0605
Rocco Chiara <rocco.r.chiara@gmail.com>
2017-04-15 22:48:47 -0400

    Added jumbotron, google fonts, font awesome icons and bootstrap grid css. Stylized.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
-            <h2>Goodbye {{name}}</h2>
-            <h3>I'm back!</h3>
-            <h4>No seriously, I'm back</h4>
-            <i class="fa fa-star fa-2x" aria-hidden="true"></i>
+  template: `
+
+<div class="jumbotron">
+  <div class="text-center">
+    <h1>Ratings</h1>
+  </div> 
+
+  <div class="text-center">
+    <i class="fa fa-star fa" aria-hidden="true"></i>
+    <i class="fa fa-star fa" aria-hidden="true"></i>
+    <i class="fa fa-star fa" aria-hidden="true"></i>
+    <i class="fa fa-star fa" aria-hidden="true"></i>
+    <i class="fa fa-star fa" aria-hidden="true"></i>
+  </div>
+</div>
   `,
   styleUrls: ['styles.css']
 })
